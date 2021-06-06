133abb9f3a9356a0e39f062dec3655d123c2d6cb
anisqureshi00 <85334058+anisqureshi00@users.noreply.github.com>
2021-06-06 05:24:43 -0800

    first commit

---
+++ about.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">MyWebsite</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+
+
+
+                    </u1>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+            </div>
+        </div>
+</nav>
+<div class="jumbotron">
+  <h1>MyWebsite</h1>
+  <p>We are php  Developer</p>
+</div>
+<section class="my-5">
+        <div class="py-5">
+            <h2 class="text-center"> About Us</h2>
+        </div>
+        <div class="container-fluids">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-12">
+                    <img src="https://source.unsplash.com/1600x900/?apple,coding" class="img-fluid aboutimg">
+                </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                    <h2 class="displayed-4">This is MyWebsite.</h2>
+                    <p class="py-3">PHP is a general-purpose scripting language especially suited to web development. It
+                        was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference
+                        implementation is now produced </p>
+                    <a href="about.php" class="btn btn-success"> check More </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <p class="p-3 bg-dark text-white text-center">@MyWebsiteproduction</p>
+    </footer>
+
+        
+    </body>
+    </html>
